b80383ea69d8a20d15726992ef2873335d75d486
ElliotCase <40835798+ElliotCase@users.noreply.github.com>
2020-08-17 20:58:26 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+Game function:
+- player must guess a number between a min and max
+- player gets a certain amount of guesses
+- notify play of guesses remaining
+- notify the player of the correct answer if loose
+- let player choose to play again
+*/
+
+// game values
+
+let min = 1,
+    max = 10,
